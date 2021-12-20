cda66439dfb32487107d75d6e4ade93dd1b5de50
Sergio Mendoza <88686703+Rospo19@users.noreply.github.com>
2021-12-20 14:12:38 -0300

    Update ejercicio5.java
    corregir

--- EjerciciosComplementariosLevel3/ejercicio5.java
+++ EjerciciosComplementariosLevel3/ejercicio5.java
@@ -1,27 +1,53 @@
-package ejercicios.complementarios.parte3;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.Period;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class ejercicio5 {
+public class Ejercicio5 {
     public static void main(String[] args) {
-        List<Alumno> alumnos =
-                List.of(
-                        new Alumno("Senor1", "Misterioso1", LocalDate.of(1934, 11, 1)),
-                        new Alumno("Senor2", "Misterioso2", LocalDate.of(1980, 2, 20)),
-                        new Alumno("Senor3", "Misterioso3", LocalDate.of(1924, 9, 1)));
+        List<Alumno> alumnos = List.of(
 
-        Map<String, Integer> edades = new HashMap<>();
+            new Alumno("Szyslak", "Moe", LocalDate.of(1967,12,25)),
+            new Alumno("Leonard", "Lenny", LocalDate.of(1972,5,12)),
+            new Alumno("Carlson", "Carl", LocalDate.of(1973,8,23)),
+            new Alumno("Patiño", "Bob", LocalDate.of(1965,4,22)),
+            new Alumno("Flanders", "Ned", LocalDate.of(1955,10,2)));
 
-        for (Alumno alumno : alumnos) {
-            edades.put(
-                    alumno.getApellido() + " " + alumno.getNombre(),
-                    alumno.getNacimiento().until(LocalDate.now()).getYears());
+            Map<String, Integer> alumnosEdad = alumnos.stream()
+            .collect(Collectors.toMap(alumno -> alumno.getNombre().concat(" ")
+            .concat(alumno.getApellido()),
+            alumno -> calcularEdad(alumno)));
+
+        System.out.println(alumnosEdad);
+    }
+
+    private static Integer calcularEdad(Alumno alumno){
+        return Period.between(alumno.getFechaDeNacimiento(), LocalDate.now()).getYears();
+    }
+
+    static class Alumno {
+        String apellido;
+        String nombre;
+        LocalDate fechaDeNacimiento ;
+
+        public Alumno(String apellido, String nombre, LocalDate fechaDeNacimiento) {
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.fechaDeNacimiento = fechaDeNacimiento;
         }
 
-        System.out.println(edades);
+        public String getApellido(){
+            return this.apellido;
+        }
+
+        public String getNombre(){
+            return this.nombre;
+        }
+
+        public LocalDate getFechaDeNacimiento(){
+            return this.fechaDeNacimiento;
+        }
     }
 }
 
