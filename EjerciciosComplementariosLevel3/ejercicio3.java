397fba49902dc55c93cce797c480b098c9b25d52
Sergio Mendoza <88686703+Rospo19@users.noreply.github.com>
2021-12-20 14:09:53 -0300

    Update ejercicio3.java
    corregir

--- EjerciciosComplementariosLevel3/ejercicio3.java
+++ EjerciciosComplementariosLevel3/ejercicio3.java
@@ -1,27 +1,16 @@
-package ejercicios.complementarios.parte3;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import java.util.List;
-
-public class ejercicio3 {
-    private static final String LETRA_INICIO = "b";
-
+public class Ejercicio3 {
     public static void main(String[] args) {
-        List<String> palabras =
-                List.of("Batman", "Aquaman", "Flash", "Batgirl", "Wonder Woman", "Brainiac");
+        List<String> palabras = List.of("Batman", "Aquaman", "Flash", "Batgirl",
+         "Wonder Woman", "brainiac");
 
+         List<String> palabrasFiltradas = palabras.stream()
+                .filter(palabra -> palabra.startsWith("B") || palabra.startsWith("b"))
+                .collect(Collectors.toList());
 
-        long count = 0;
-        for (String pal : palabras) {
-            if (filtrarPorInicio(pal)) {
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
-
-    private static boolean filtrarPorInicio(String s) {
-        return s != null && !s.isEmpty() && LETRA_INICIO.equalsIgnoreCase(s.substring(0, 1));
+        System.out.println(palabrasFiltradas);
     }
 }
-
 
