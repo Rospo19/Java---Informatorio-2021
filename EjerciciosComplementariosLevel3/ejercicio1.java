9e078612242849518e671885f5f1ee382801c656
Sergio Mendoza <88686703+Rospo19@users.noreply.github.com>
2021-12-20 14:07:29 -0300

    Update ejercicio1.java
    corregir

--- EjerciciosComplementariosLevel3/ejercicio1.java
+++ EjerciciosComplementariosLevel3/ejercicio1.java
@@ -1,23 +1,19 @@
-package ejercicios.complementarios.parte3;
+import java.util.*;
+import java.util.stream.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ejercicio1 {
+public class Ejercicio1 {
     public static void main(String[] args) {
         List<String> palabras = new ArrayList<>();
-        palabras.add("hola");
-        palabras.add("");
+        palabras.add("Hola");
         palabras.add(null);
-        palabras.add("informatorio");
+        palabras.add("Informatorio");
+        palabras.add("");
 
-        List<String> endPalabras = new ArrayList<>();
-        for (String s : palabras) {
-            if (s != null && !s.isEmpty()) {
-                endPalabras.add(s);
-            }
-        }
-        System.out.println(endPalabras);
+        List<String> palabrasFiltradas = palabras.stream()
+                .filter(palabra -> palabra != "" && palabra != null)
+                .collect(Collectors.toList());
+
+        System.out.println(palabrasFiltradas);
     }
 }
 
